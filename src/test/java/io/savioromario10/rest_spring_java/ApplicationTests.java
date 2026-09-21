1e75e76dfaa3081f13cc854dd8c8d5ea42618019
Sávio Romário <savioromario10@gmail.com>
2026-09-21 15:23:10 -0300

    first commit

---
+++ src/test/java/io/savioromario10/rest_spring_java/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.savioromario10.rest_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
